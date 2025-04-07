070bc2d11c31c2912f4fe5be78a0fe8a44afe0eb
isaquesousa22 <165092549+isaquesousa22@users.noreply.github.com>
2025-04-07 20:06:27 -0300

    Add files via upload

---
+++ funcao_soma.php
@@ -0,0 +1,23 @@
+<?php 
+
+
+$num1 = 10;
+$num2 = 20; 
+function somaValor($v1,  $v2 ){
+
+	
+	return($v1 + $v2);
+
+
+}
+ $resp = somaValor($num1,  $num2 );
+
+echo " O resultado da soma de $num1 + $num2 é " .$resp;
+
+
+
+
+
+
+
+ ?>
