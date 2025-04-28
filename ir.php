c48e04de7a02bd2c170073017e9d7026fbf866cc
Kvsvicentini <kaua.vicentini@g,ail.com>
2025-04-28 20:28:00 -0300

    blabla

--- ir.php
+++ ir.php
@@ -1,34 +1,54 @@
 <?php 
 
-$salario = 5000.00; 
+$salary = 3000.00; 
+echo "Salário bruto: R$$salary";
 
-function calculoRenda(){
+function calculoRenda($salary){
 
-global $salario;
 
-if($salario >  3036 && $salario < 3533.31){
-	$pd = $salario*7.5/100;
-	$sl = $salario - $pd; 
+
+ 	if($salary >  2259.21 && $salary < 2826.65){
+		$discount = $salary * (7.5/100);
+		echo "<br/>";
+	}
+
+ 	elseif($salary > 2826.66 && $salary < 3751.05){
+		$discount = $salary * (15/100);
+		echo "<br/>";
+	}
+
+	 else if($salary >  3751.06 && $salary < 4664.68 ){
+		$discount = $salary * (22.5/100);	
+		echo "<br/>";
+	}
+
+ 	else if($salary > 4664.68 ){
+		$discount = $salary * (27.5/100);
+		echo "<br/>";
+	}
+
+	else {
+		$discount = $salary * (27.5/100);
+		$sal_liq = $salary - $discount; 
+		echo "<br/>";
+
+		$sal_liq = salary - $discount;
+		return $sal_liq;
+	}
+
+
 }
 
-if($salario > 3533.32 && $salario < 4688.85){
-	$pd = $salario*15/100;
-	$sl = $salario - $pd; 
-}
+$salarioLiquido = calculoRenda(salary);
 
-if($salario > 4688.86 && $salario < 5830.85 ){
-	$pd = $salario*22.5/100;
-	$sl = $salario - $pd; 
-}
-
-if($salario > 5830.85 ){
-	$pd = $salario*27.5/100;
-	$sl = $salario - $pd; 
+function impress($r){
+ return "salarioLiquido";
 }
 
 
-} 
-
+echo "<br/>";
+	
+ impress ($salarioLiquido); 
 
 
 
