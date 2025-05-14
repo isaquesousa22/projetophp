d7c2f000d97dff21996c8703cdbf6c19a8195f99
isaquesousa22 <165092549+isaquesousa22@users.noreply.github.com>
2025-05-13 22:12:16 -0300

    Add files via upload

---
+++ array.php
@@ -0,0 +1,25 @@
+<?php 
+
+$nome = array();
+
+
+$nome[0] = 'Etecia';
+$nome[1] = 'Irmã';
+$nome[2] = 'Agostina';
+
+$num = 10;
+
+print_r($nome);
+
+echo "<br/>";
+
+var_dump($nome);
+
+echo "<br/>";
+
+print_r($num);
+
+
+
+
+ ?>
---
+++ veiculos.php
@@ -0,0 +1,15 @@
+<?php 
+
+
+$carros = array('Mustang', 'Mercedez c200', 'opa 1969', 'Supra');
+
+
+$carros[] = 'Audie Q4';
+
+var_dump($carros);
+
+echo'<br/>';
+
+print_r($carros);
+
+ ?>
